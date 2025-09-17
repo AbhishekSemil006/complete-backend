06de3c0447f5159163d63fc8bad89a60262d7580
AbhishekSemil006 <145631264+AbhishekSemil006@users.noreply.github.com>
2025-09-17 12:05:09 +0530

    login and logout user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,28 @@ import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import {ApiResponse} from '../utils/ApiResponse.js'
 
+
+const generateAccessAndRefreshTokens = async(userId) =>
+{
+    try{
+        const user = await User.findById(userId)
+        const accessToken =  user.generateAccessToken()
+        const refreshToken =  user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+
+    } catch(error){
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
+
+//register user.
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend.
     // validation - not empty
@@ -95,6 +117,90 @@ const registerUser = asyncHandler(async (req, res) => {
 
 } )
 
+//login user.
+const loginUser = asyncHandler(async (req, res) => {
+    // get user details from frontend.
+    // validation not empty.
+    // eamil,password - validation.
+    //check if user exists.
+    //check for email and password is correct.  
+    //generate access and refresh token.
+    //send cookies.
+    //return response.
+    const {email, username, password} = req.body
+
+    if(!username || !email) {
+        throw new ApiError(400,"username or email is required")    
+    }
+     
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user) {
+        throw new ApiError(404, "User not found,")
+    }
+    
+    const isPasswordCorrect = await user.isPasswordCorrect(password)
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(401, "Password is incorrect")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        ) 
+    )
+
+
+})
+
+//logout user.
+const logoutUser = asyncHandler(async(req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+        $set: {
+            refreshToken: undefined
+        }
+    },
+    {
+        new: true
+    }
+  )
+
+  const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out Successfully"))
+})
+
 export { 
     registerUser,
+    loginUser,
+    logoutUser
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,31 +1,47 @@
 import { Router  } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-// router.route("/register").post(
-//     upload.fields([
-//         {
-//             name: "avatar",
-//             maxCount: 1
-//         },
-//         {
-//             name: "coverImage",
-//             maxCount:1
-//         }
-//     ]),
-//     registerUser
-// 
-
-router.post(
-  "/register",
-  upload.fields([
-    { name: "avatar", maxCount: 1 },
-    { name: "coverImage", maxCount: 1 }
-  ]),
-  registerUser
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
 );
 
+router.route("/login").post(loginUser)
 
-export  default router
+
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+
+
+
+
+
+
+
+
+
+// router.post(
+//   "/register",
+//   upload.fields([
+//     { name: "avatar", maxCount: 1 },
+//     { name: "coverImage", maxCount: 1 }
+//   ]),
+//   registerUser
+// );
+
+
+export default router
