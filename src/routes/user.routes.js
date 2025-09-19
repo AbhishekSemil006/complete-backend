126a61cc1189ab403846e4d6c67cde7cf285b083
AbhishekSemil006 <145631264+AbhishekSemil006@users.noreply.github.com>
2025-09-19 22:25:15 +0530

    some bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import { Router  } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { loginUser } from "../controllers/user.controller.js";
+import { logoutUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -19,7 +21,7 @@ router.route("/register").post(
     registerUser
 );
 
-router.route("/login").post(loginUser)
+router.route("/loginUser").post(loginUser)
 
 
 
